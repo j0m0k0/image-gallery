df66ea512a750093a0bbbb085b1ee0578e3f2109
Jxxxx <93967783+j0m0k0@users.noreply.github.com>
2022-01-04 03:50:59 +0330

    Delete pg-front directory

--- pg-front/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import generalReducer from "./redux/generalSlice";
-
-export const store = configureStore({
-  reducer: {
-    general: generalReducer,
-  },
-});
